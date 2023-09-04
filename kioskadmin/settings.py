c0a24dcb2109c3392e8b253aca5bea35e57f8b54
Youngbin Han <ybhan@ubuntu.com>
2023-09-04 19:29:38 +0900

    pgdql setup

--- kioskadmin/settings.py
+++ kioskadmin/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'rest_framework.authtoken',
+    "rest_framework",
+    "rest_framework.authtoken",
     "kiosksvc",
-    'import_export'
+    "import_export",
 ]
 
 MIDDLEWARE = [
@@ -92,8 +92,12 @@ WSGI_APPLICATION = "kioskadmin.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASS"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
 

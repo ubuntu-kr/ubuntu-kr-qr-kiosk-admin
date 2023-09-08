51b00cf3dda70485acf1c4fbb9272e3e01c1596e
Youngbin Han <youngbin.han@cloudmt.co.kr>
2023-09-09 00:42:38 +0900

    Add __str__ for models

--- kiosksvc/models.py
+++ kiosksvc/models.py
@@ -9,8 +9,12 @@ class Participant(models.Model):
     qrUrl = models.URLField()
     couponDetail = models.CharField(max_length=200, default="")
     passCode = models.BinaryField(max_length=100, null=True)
+    def __str__(self):
+        return f"{self.name}({self.email})"
 
 class CheckInLog(models.Model):
     tokenId = models.CharField(max_length=100, unique=True)
     checkedInAt = models.DateTimeField()
     participant = models.ForeignKey(Participant, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return f"{self.tokenId}({self.checkedInAt})"
